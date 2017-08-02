3f2ec6185c2a4458b9f83bcc1e570f950051c45e
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-02 14:29:56 -0500

    fixing bugs y Todo sin conectar con Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,53 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+  showDialog = false;
+  editingTodo: any = null;
+  fieldValue = '';
+  todoList: any = [];
+  okButtonText = 'Crear Tarea';
+
+  todoDialog(todo = null) {
+    this.okButtonText = 'Crear Tarea';
+    this.fieldValue = '';
+    this.editingTodo = todo;
+    if (todo) {
+      this.fieldValue = todo.title;
+      this.okButtonText = 'Editar';
+    }
+    this.showDialog = true;
+  }
+
+  remove(index: number) {
+    this.todoList.splice(index, 1);
+  }
+
+  editTodo(title) {
+    this.editingTodo.title = title;
+  }
+
+  updateTodo(title) {
+    if (title) {
+      title = title.trim();
+      if (this.editingTodo) {
+        this.editTodo(title);
+      } else {
+        this.addTodo(title);
+      }
+    }
+    this.hideDialog();
+  }
+
+  addTodo(title) {
+    const todo = {title: title, completed: false};
+    this.todoList.push(todo);
+  }
+
+  hideDialog() {
+    this.showDialog = false;
+    this.editingTodo = null;
+    this.fieldValue = null; // make sure Input is new
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
+import { FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
+
+import 'hammerjs'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
